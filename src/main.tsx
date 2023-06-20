74ea9850d1dc1686d2f3af1242a17f31e1e82392
Jade.Kim <jade.kim@travelflan.com>
2023-06-20 21:15:24 +0100

    Remove unused files

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-// import App from './App.tsx';
 import Archive from './pages/Archive.tsx';
 import Recording from './pages/Recording.tsx';
 import Visualisation from './pages/Visualisation.tsx';
 import './index.css';
-
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import "./index.css";
 
 const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <App />,
-  // },
   {
     path: "/",
     element: <Recording />,
