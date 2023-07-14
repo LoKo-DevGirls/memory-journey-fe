c4abfc51d5e9b676a52c248b5bc099161d55c9d4
Jade.Kim <jade.kim@travelflan.com>
2023-07-14 20:01:38 +0100

    WIP

--- src/pages/Archive.tsx
+++ src/pages/Archive.tsx
@@ -1,5 +1,11 @@
+import { ForceGraph3D } from 'react-force-graph';
+import mockupdata from '../assets/mockupdata.json';
+import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
+
+const extraRenderers = [new CSS2DRenderer()];
+
 // import axios from 'axios';
-// import { useEffect, useState } from 'react';
+import { useRef, useCallback } from 'react';
 
 function Archive() {
   // const [archiveData, setArchiveData] = useState([]);
@@ -11,11 +17,40 @@ function Archive() {
   //   return () => {
   //   }
   // }, [])
-  
+
+  const fgRef = useRef<any>();
+
+  const handleClick = useCallback((node: any) => {
+    // Aim at node from outside it
+    const distance = 40;
+    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+    
+    fgRef.current.cameraPosition(
+      { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+      node, // lookAt ({ x, y, z })
+      3000  // ms transition duration
+    );
+    
+  }, [fgRef]);
+
   return (
-    <>
-      <h1>Archive</h1>
-    </>
+    <div>
+      <ForceGraph3D
+        extraRenderers={extraRenderers}
+        graphData={mockupdata}
+        nodeAutoColorBy="group"
+        nodeThreeObject={(node: any) => {
+          const nodeEl = document.createElement('div');
+          nodeEl.textContent = node.id;
+          nodeEl.style.color = node.color;
+          nodeEl.className = 'node-label';
+          return new CSS2DObject(nodeEl);
+        }}
+        nodeThreeObjectExtend={true}
+        ref={fgRef}
+        onNodeClick={handleClick}
+        />
+    </div>
   )
 }
 
