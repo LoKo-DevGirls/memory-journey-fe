b5296d542bcecc52c67aaf665d9c89e8fd774ec2
Jade.Kim <jade.kim@travelflan.com>
2023-07-14 17:52:37 +0100

    FIx link

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,9 +3,9 @@ function Landing() {
   return (
     <div className="container">
       <div className="container">
-        <a href="/recording">Record your memories</a>
-        <a href="/archive">Memory archives</a>
-        <a href="/research">Research</a>
+        <a href="/recording/">Record your memories</a>
+        <a href="/archive/">Memory archives</a>
+        <a href="/research/">Research</a>
       </div>
     </div>
   )
