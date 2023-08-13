2ab1bd97626c0ba86caf792017ebd24358f65013
Jade.Kim <jade.kim@travelflan.com>
2023-08-13 17:21:06 +0100

    Uncomment links

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -19,8 +19,8 @@ function Landing() {
         </p>
 
         <a href="/recording/">Record your Memories</a>
-        {/* <a href="/archive/">Memory Archives</a>
-        <a href="/research/">Research</a> */}
+        <a href="/archive/">Memory Archives</a>
+        <a href="/research/">Research</a>
       </div>
     </div>
   )
