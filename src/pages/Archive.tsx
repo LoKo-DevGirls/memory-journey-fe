587c1f2ee76d8d7a68b9735882feaa0d49042234
Jade.Kim <jade.kim@travelflan.com>
2023-08-18 17:54:23 +0100

    Draw geometry from link group data

--- src/pages/Archive.tsx
+++ src/pages/Archive.tsx
@@ -1,16 +1,27 @@
-import { useRef, useCallback } from 'react';
-import { ForceGraph3D } from 'react-force-graph';
-import mockupdata from '../assets/mockupdata.json';
+import { useRef, useCallback, useMemo, useState } from 'react';
+import { ForceGraph3D } from 'react-force-graph'; // https://github.com/vasturiano/react-force-graph
+import sampledata from '../dataset/sampledata.json';
 import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
 import {Object3D, Vector3} from 'three';
 import SpriteText from 'three-spritetext';
 import * as THREE from 'three';
+import {genRandomTree} from '../utils/randomData.js';
 
 const extraRenderers = [new CSS2DRenderer() as any];
 
 function Archive() {
-
   const fgRef = useRef<any>();
+
+  const groupData = useMemo(() => {
+    return sampledata.groups
+  }, []);
+  const linksData = useMemo(() => {
+    return sampledata.links
+  }, []);
+  const nodesData = useMemo(() => {
+    return sampledata.nodes
+  }, []);
+  const [geometry, setGeometry] = useState();
 
   const handleClick = useCallback((node: any) => {
     // Aim at node from outside it
@@ -26,25 +37,30 @@ function Archive() {
   }, [fgRef]);
 
   const nodeThreeObject = (node: any) => {
-    const nodeEl = document.createElement('div');
-    nodeEl.textContent = node.id;
-    nodeEl.style.color = node.color;
-    nodeEl.className = 'node-label';
-    return new CSS2DObject(nodeEl);
+    const geometryGroup = new THREE.Group();
+    const { groupIds } = node;
+    groupIds.forEach((id: number):void => {
+      geometryGroup.add(getGeometryFromGroupId(id));
+    });
+
+    return geometryGroup
   }
 
-  const getGeometry = (link:any) => {
-    const { source,target } = link
+  const getGeometryFromGroupId = (groupId: number) => {
+    const targetGroup = groupData.filter(g => g.groupId === groupId)[0];
+    const { links } = targetGroup;
+    const linkObjectList = links.map(id => (
+      linksData.filter(link => link.linkId === id)[0]
+    ));
 
     const material = new THREE.MeshNormalMaterial({side:THREE.DoubleSide})
     let geometry = new THREE.BufferGeometry()
-    const points = [
-      new THREE.Vector3(source.x,source.y,source.z), 
-      new THREE.Vector3(target.x, target.y, target.z),
-      // ... TODO: Add more points to draw surface
-      // new THREE.Vector3(200, 200, 200),
-      // TODO: Add case when link group cannot draw surface (group less than 3)
-    ]
+    
+    const points:any = [];
+    linkObjectList.forEach(linkObj => {
+      points.push(new THREE.Vector3(linkObj.source.x, linkObj.source.y, linkObj.source.z))
+      points.push(new THREE.Vector3(linkObj.target.x, linkObj.target.y, linkObj.target.z))
+    })
 
     geometry.setFromPoints(points)
     geometry.computeVertexNormals()
@@ -53,35 +69,22 @@ function Archive() {
     return mesh
   }
 
-  const linkThreeObject = (link: any) => {
-    return getGeometry(link)
-  }
-
-  // const linkPositionUpdate = (sprite: Object3D<THREE.Event>, { start, end }: { start: any; end: any; }) => {
-  //   const middlePos = Object.assign(...['x', 'y', 'z'].map(c => ({
-  //     [c]: start[c] + (end[c] - start[c]) / 2 // calc middle point
-  //   })));
-
-  //   Position sprite
-  //   Object.assign(sprite.position, middlePos);
-  // }
-
   return (
     <div>
       <ForceGraph3D
         extraRenderers={extraRenderers}
-        graphData={mockupdata}
+        graphData={sampledata}
         nodeAutoColorBy="group"
         nodeThreeObject={nodeThreeObject}
         nodeThreeObjectExtend={true}
+        nodeOpacity={0.5}
         ref={fgRef}
         onNodeClick={handleClick}
-        linkVisibility={false}
+        linkVisibility={true}
         nodeRelSize={1}
         nodeColor={'white'}
         linkThreeObjectExtend={true}
-        linkThreeObject={linkThreeObject}
-        // linkPositionUpdate={linkPositionUpdate}
+        // linkThreeObject={linkThreeObject}
       />
     </div>
   )
