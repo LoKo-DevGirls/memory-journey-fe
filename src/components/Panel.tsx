af7cdd50b64666dac1dc355ff2a66dd611e569de
Jade.Kim <jade.kim@travelflan.com>
2023-12-12 18:48:42 +0000

    Add panel

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -8,7 +8,14 @@ function Panel({ children }: any) {
   return (
     <div className={styles.panel}>
       <button {...getToggleProps()}>
-        {isExpanded ? 'Collapse' : 'Expand'}
+        <span>Legend</span>
+        {isExpanded ? 
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18 15L12 9L6 15" stroke="#f2f2f2"/></svg> 
+        : 
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18 9L12 15L6 9" stroke="#f2f2f2"/>
+        </svg>}
       </button>
       <section {...getCollapseProps()}>{children}</section>
     </div>
